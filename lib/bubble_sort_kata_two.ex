254192f93ccd9243503161085d27dc365c63c199
Daisy Molving <daisymolving@gmail.com>
2016-10-13 13:46:49 +0100

    swaps two numbers if they are descending

--- lib/bubble_sort_kata_two.ex
+++ lib/bubble_sort_kata_two.ex
@@ -1,6 +1,14 @@
 defmodule BubbleSortKata do
 
-  def sort(numbers) do
-    numbers
+  def sort([current_number, next_number]) do
+    swap(current_number, next_number)
+  end
+
+  def swap(current_number, next_number) do
+    if current_number <= next_number do
+      [current_number, next_number]
+    else
+      [next_number, current_number]
+    end
   end
 end
--- test/bubble_sort_kata_two_test.exs
+++ test/bubble_sort_kata_two_test.exs
@@ -4,4 +4,9 @@ defmodule BubbleSortKataTest do
   test "a list of two numbers that is already ascending does not change" do
     assert BubbleSortKata.sort([1, 2]) == [1, 2]
   end
+
+  test "a list of two numbers are are descending are swapped" do
+    assert BubbleSortKata.sort([2, 1]) == [1, 2]
+  end
+
 end
